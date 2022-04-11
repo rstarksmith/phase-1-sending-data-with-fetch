55df2435c47ab23f0563da8ccc12128a337d428b
Rachel <rstark.smith@gmail.com>
2022-04-11 10:38:25 -0500

    commit tree 40e773e09bfe17fbf64577fdd9ba12e6cb978536

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Add your code here
+
+
+function submitData(userName, userEmail) {
+    const handleData = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+    },
+    body: JSON.stringify({
+        name: userName,
+        email: userEmail,
+    })}
+    return fetch('http://localhost:3000/users', handleData)
+
+    .then(response => response.json())
+    .then(function (object) {
+    const body = document.querySelector('body')
+    const p = document.createElement('p')
+    p.textContent = object.id
+    body.appendChild(p)
+    })
+
+    .catch(function(error) {
+        console.log(error.message)
+        const body = document.querySelector('body')
+        const div = document.createElement('div')
+        div.textContent = error.message
+        body.appendChild(div)
+
+    })
+}
+
+// submitData("Jerry", "jerry@gmail.com")
+
+
+
+
+
+    
